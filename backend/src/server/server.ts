fabad016ecea174b1881982d49e61368bba7d1f9
Judith <mariajudith.vargas@ucr.ac.cr>
2025-03-21 22:50:36 -0600

    initial-commit-Frontend

--- backend/src/server/server.ts
+++ backend/src/server/server.ts
@@ -1,12 +1,15 @@
 import routes from "@routes/routes";
 import express, { Application} from "express";
 import morgan from "morgan";
-
+import cors from "cors";
 const app: Application = express();
 
 app.use(express.json()); 
 app.use(morgan("dev"));
-
+app.use(cors({
+    origin: "http://localhost:4200", // Permitir solo el frontend
+    credentials: true // Si usas cookies o autenticación
+  }));
 app.use("/api", routes());
 
 export default app;
---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthComponent } from './components/auth/auth.component';
+import { LoginComponent } from './components/auth/login/login/login.component';
+import { RegisterComponent } from './components/auth/register/register/register.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AuthComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
